8443c0527075ce968c4370ccc5ac7e8f80c7f040
Claudia Pinto <claudiaacpinto@gmail.com>
2025-09-03 20:12:35 +0000

    Update models to align correctly with the API response

--- Challenge-Cat-API/Core/Models/CatModels.swift
+++ Challenge-Cat-API/Core/Models/CatModels.swift
@@ -9,24 +9,56 @@ import Foundation
 
 public struct Cat: Identifiable, Equatable, Decodable {
     public let id: String?
-    let url: String?
-    let breed: CatBreed?
+    public let url: String?
+    public let width: Int?
+    public let height: Int?
+    public let breeds: [CatBreed]?
+    public let uiID = UUID()
+    public var displayID: UUID { uiID }
 }
 
 extension Cat {
     init(entity: CatEntity) {
-        self.id = entity.id ?? ""
+        self.id = entity.id ?? UUID().uuidString
         self.url = entity.url ?? ""
-        self.breed = nil 
+        self.width = Int(entity.width)
+        self.height = Int(entity.height)
+        
+        if let breedEntities = entity.breed as? Set<BreedEntity> {
+            self.breeds = breedEntities.map { CatBreed(entity: $0) }
+        } else {
+            self.breeds = []
+        }
     }
 }
 
 public struct CatBreed: Identifiable, Equatable, Decodable {
-    public let id: String?
-    let  name: String?
-    let  origin: String?
-    let  temperament: String?
-    let  descriptionText: String?
-    let  lifeSpan: String?
-    let  wikipediaUrl: String?
+    public let id: String
+    public let name: String
+    let origin: String?
+    let temperament: String?
+    let descriptionText: String?
+    let lifeSpan: String?
+    let wikipediaUrl: String?
+    let countryCode: String?
+    let weight: CatBreedWeight?
+}
+
+public struct CatBreedWeight: Equatable, Decodable {
+    let imperial: String?
+    let metric: String?
+}
+
+extension CatBreed {
+    init(entity: BreedEntity) {
+        self.id = entity.id ?? ""
+        self.name = entity.name ?? ""
+        self.origin = entity.origin
+        self.temperament = entity.temperament
+        self.descriptionText = entity.descriptionText
+        self.lifeSpan = entity.lifeSpan
+        self.wikipediaUrl = entity.wikipediaUrl
+        self.countryCode = entity.countryCode
+        self.weight = CatBreedWeight(imperial: entity.weightImperial, metric: entity.weightMetric)
+    }
 }
