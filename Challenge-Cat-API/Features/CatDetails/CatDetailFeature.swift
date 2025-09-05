5a81418514dc16ed0d645007bca2d5ede05ae8b1
Claudia Pinto <claudiaacpinto@gmail.com>
2025-09-05 00:18:44 +0000

    implement detail feature

--- Challenge-Cat-API/Features/CatDetails/CatDetailFeature.swift
+++ Challenge-Cat-API/Features/CatDetails/CatDetailFeature.swift
@@ -5,3 +5,32 @@
 //  Created by Claudia Pinto - Pessoal on 04/09/2025.
 //
 
+import SwiftUI
+import ComposableArchitecture
+import Foundation
+
+public struct CatDetailFeature: Reducer {
+    public struct State: Equatable {
+        let id: UUID
+        let cat: Cat
+        var isFavorite: Bool
+       
+        @PresentationState var alert: AlertState<Action>?
+    }
+
+    public enum Action: Equatable {
+        case toggleFavorite
+        case alert(PresentationAction<Action>)
+    }
+
+    public func reduce(into state: inout State, action: Action) -> Effect<Action> {
+        switch action {
+        case .toggleFavorite:
+            state.isFavorite.toggle()
+            return .none
+
+        case .alert:
+            return .none
+        }
+    }
+}
